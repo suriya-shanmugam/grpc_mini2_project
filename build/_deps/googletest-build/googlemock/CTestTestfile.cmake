333f0dd1903c46b5ef6ef149f63afbcfc52bb0d8
suriya-shanmugam <suriya.shanmugam.us@gmail.com>
2025-03-06 13:39:30 -0800

    removed old files

--- build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/suriya-018231499/sem2/cmpe273/mini2/collisionproject/275_mini1/temp2/build/_deps/googletest-src/googlemock
-# Build directory: /home/suriya-018231499/sem2/cmpe273/mini2/collisionproject/275_mini1/temp2/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
